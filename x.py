56174fc492090db47e6761c270c8d577fb67ccd5
eli <eliplutchok1@gmail.com>
2025-08-18 22:32:42 -0400

    added figure 13.2

--- x.py
+++ x.py
@@ -6,11 +6,16 @@ from autograd import grad
 states = np.arange(0, 3)
 actions = ['left', 'right']
 
-def feature_vector(s, a):
+def action_feature_vector(s, a):
     if a == 'left':
         return np.array([0, 1])
     else:
         return np.array([1, 0])
+
+def state_feature_vector(s):
+    vec = np.zeros(3)
+    vec[s] = 1
+    return vec
 
 def get_next_state_and_reward(s, a):
     direction = 1 if a == 'right' else -1
@@ -22,9 +27,12 @@ def get_next_state_and_reward(s, a):
     return next_state, -1
 
 def pi(s, a, theta):
-    return np.exp(np.dot(theta, feature_vector(s, a))) / np.sum([np.exp(np.dot(theta, feature_vector(s, act))) for act in actions])
+    return np.exp(np.dot(theta, action_feature_vector(s, a))) / np.sum([np.exp(np.dot(theta, action_feature_vector(s, act))) for act in actions])
 
-def generate_episode(theta, max_steps=1000):
+def v(s, w):
+    return np.dot(w, state_feature_vector(s))
+
+def generate_episode(theta, max_steps=300):
     episode = []
     state = 0
     while state != 3 and len(episode) < max_steps:
@@ -37,34 +45,51 @@ def generate_episode(theta, max_steps=1000):
 def reinforce(policy, alpha = 2 ** -12, gamma = 1.0, num_episodes = 1000):
     returns = []
 
-    W = np.zeros(2)
+    theta = np.array([2.0, -2.0])
     for _ in range(num_episodes):
-        frozen_W = W.copy()
-        episode = generate_episode(frozen_W)
+        frozen_theta = theta.copy()
+        episode = generate_episode(frozen_theta)
         G = 0
         for state, action, reward in reversed(episode):
             G = gamma * G + reward
             logpi = lambda theta: np.log(policy(state, action, theta))
-            grad_logpi = grad(logpi)(frozen_W)
-            W += alpha * G * grad_logpi
+            grad_logpi = grad(logpi)(frozen_theta)
+            theta += alpha * G * grad_logpi
         returns.append(G)
-    return W, returns
+    return theta, returns
 
-def get_100_run_return_avg(alpha):
+def reinforce_with_baseline(policy, alpha_theta = 2 ** -9, alpha_w = 2 ** -6, gamma = 1.0, num_episodes = 1000):
+    returns = []
+
+    theta = np.array([2.0, -2.0])
+    w = np.array([0.0, 0.0, 0.0])
+    for _ in range(num_episodes):
+        frozen_theta = theta.copy()
+        frozen_w = w.copy()
+        episode = generate_episode(frozen_theta)
+        G = 0
+        for state, action, reward in reversed(episode):
+            G = gamma * G + reward
+            delta = G - v(state, frozen_w)
+            w += alpha_w * delta * state_feature_vector(state)
+            logpi = lambda theta: np.log(policy(state, action, theta))
+            grad_logpi = grad(logpi)(frozen_theta)
+            theta += alpha_theta * delta * grad_logpi
+        returns.append(G)
+    return theta, returns
+
+def get_100_run_return_avg(learning_algo):
     all_run_returns = []
     for _ in range(100):
-        _, episode_returns = reinforce(pi, alpha = alpha)
+        _, episode_returns = learning_algo(pi)
         all_run_returns.append(onp.asarray(episode_returns))
     return onp.mean(onp.stack(all_run_returns, axis=0), axis=0)
 
-returns_12 = get_100_run_return_avg(2 ** -12)
-returns_13 = get_100_run_return_avg(2 ** -13)
-returns_14 = get_100_run_return_avg(2 ** -14)
-
-plt.plot(returns_12, label='alpha = 2 ** -12')
-plt.plot(returns_13, label='alpha = 2 ** -13')
-plt.plot(returns_14, label='alpha = 2 ** -14')
-plt.axhline(y=-11, color='black', linestyle='--')
+returns_13_reinforce = get_100_run_return_avg(reinforce)
+returns_13_reinforce_with_baseline = get_100_run_return_avg(reinforce_with_baseline)
+plt.plot(returns_13_reinforce_with_baseline, label='with baseline', color='green', linewidth=1)
+plt.plot(returns_13_reinforce, label='without baseline', color='red', linewidth=1)
+plt.axhline(y=-11, color='black', linestyle='--', linewidth=1)
 plt.ylim(-100, 0)
 plt.legend()
 plt.show()
